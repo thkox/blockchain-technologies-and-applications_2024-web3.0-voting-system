73249aa0c4120d53640e83c04ad107b47b2ba40b
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-07-12 14:10:57 +0300

    Basic Stracture for the smart contract

---
+++ contracts/1_VotingSystem.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.8.2 <0.9.0;
+
+contract VotingSystem {
+
+    function createNewPoll() public {
+        
+    }
+
+    function vote() public {
+        
+    }
+
+    function closePoll() public {
+
+    }
+
+    function showPollResults() public {
+
+    }
+}
