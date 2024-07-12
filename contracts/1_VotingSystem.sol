a017e870e3c002fed505bf6ff5e780f36818aa77
Theodoros Koxanoglou <theokoxan@gmail.com>
2024-07-12 14:26:35 +0300

    - update struct Poll - users are unique

--- contracts/1_VotingSystem.sol
+++ contracts/1_VotingSystem.sol
@@ -1,9 +1,16 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract VotingSystem {
+    struct Poll {
+        string question;
+        bool isClosed;
+        mapping(address => bool) hasVoted;  // unique voters
+        uint256 yesVotes;
+        uint256 noVotes;
+    }
 
     function createNewPoll() public {
-        
+
     }
 
     function vote() public {
